a3be6a321ef189eef6f1b44fb689964185eae1d5
Zwb <wenbo.zhang@tendcloud.com>
2016-03-26 09:45:12 +0800

    Finish update

--- src/module.cc
+++ src/module.cc
@@ -4,279 +4,308 @@
 #include "producer.h"
 #include "consumer.h"
 
-using namespace std;
-using namespace v8;
-using namespace node;
+using Nan::FunctionCallbackInfo;
+using Nan::AsyncQueueWorker;
+using Nan::GetCurrentContext;
+using Nan::MakeCallback;
+using Nan::HandleScope;
+using Nan::GetFunction;
+using Nan::AsyncWorker;
+using Nan::CopyBuffer;
+using Nan::Utf8String;
+using Nan::Persistent;
+using Nan::ObjectWrap;
+using Nan::NewBuffer;
+using Nan::Callback;
+using Nan::Null;
+using Nan::Set;
+using Nan::New;
+
+using v8::FunctionTemplate;
+using v8::Function;
+using v8::Integer;
+using v8::String;
+using v8::Object;
+using v8::Number;
+using v8::Local;
+using v8::Value;
+
+using std::cout;
+using std::endl;
+using std::map;
+
+using node::Buffer::HasInstance;
+using node::Buffer::Data;
+using node::Buffer::Length;
 
 enum DataType {
-    STRING_TYPE = 1,
-    BUFFER_TYPE
+  STRING_TYPE = 1,
+  BUFFER_TYPE
 };
 
 template<DataType DT> class BatchWrap {
-  public:
-    void push(const Local<Value>& val);
+public:
+  void push(const Local<Value>& val);
 
-  public:
-    Producer::BatchType& get() {
-        return _batch;
-    }
+public:
+  Producer::BatchType& get() {
+    return _batch;
+  }
 
-    void reserve(size_t sz) {
-        _batch.reserve(sz);
-    }
+  void reserve(size_t sz) {
+    _batch.reserve(sz);
+  }
 
-  private:
-    Producer::BatchType _batch;
+private:
+  Producer::BatchType _batch;
 };
 
 template<> void BatchWrap<STRING_TYPE>::push(const Local<Value>& val) {
-    v8::Local<v8::String> toStr = val->ToString();
-    size_t size = toStr->Utf8Length();
-    Producer::ItemType item = Producer::ItemType::create(size + 1);
-    toStr->WriteUtf8(item.data());
-    _batch.push_back(std::move(item));
+  v8::Local<v8::String> toStr = val->ToString();
+  size_t size = toStr->Utf8Length();
+  Producer::ItemType item = Producer::ItemType::create(size + 1);
+  toStr->WriteUtf8(item.data());
+  _batch.push_back(std::move(item));
 }
 
 template<> void BatchWrap<BUFFER_TYPE>::push(const Local<Value>& val) {
-    _batch.push_back(Producer::ItemType(node::Buffer::Data(val), node::Buffer::Length(val)));
+  _batch.push_back(Producer::ItemType(node::Buffer::Data(val), node::Buffer::Length(val)));
 }
 
 class ProducerWrap : public ObjectWrap {
-  public:
-    static void setup(Handle<Object>& exports) {
-        const char* className = "Producer";
+public:
+  static void setup(Local<Object> exports) {
+    const char* className = "Producer";
 
-        Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(ProducerWrap::ctor);
-        tpl->SetClassName(NanNew(className));
-        tpl->InstanceTemplate()->SetInternalFieldCount(1);
+    Local<FunctionTemplate> tpl = New<FunctionTemplate>(ctor);
+    tpl->SetClassName(New(className).ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-        NODE_SET_PROTOTYPE_METHOD(tpl, "pushString", ProducerWrap::push<STRING_TYPE>);
-        NODE_SET_PROTOTYPE_METHOD(tpl, "pushBuffer", ProducerWrap::push<BUFFER_TYPE>);
+    SetPrototypeMethod(tpl, "pushString", push<STRING_TYPE>);
+    SetPrototypeMethod(tpl, "pushBuffer", push<BUFFER_TYPE>);
 
-        NODE_SET_PROTOTYPE_METHOD(tpl, "pushString2Cache", ProducerWrap::push2Cache<STRING_TYPE>);
-        NODE_SET_PROTOTYPE_METHOD(tpl, "pushBuffer2Cache", ProducerWrap::push2Cache<BUFFER_TYPE>);
+    SetPrototypeMethod(tpl, "pushString2Cache", push2Cache<STRING_TYPE>);
+    SetPrototypeMethod(tpl, "pushBuffer2Cache", push2Cache<BUFFER_TYPE>);
 
-        exports->Set(NanNew(className), tpl->GetFunction());
+    exports->Set(New(className).ToLocalChecked(), tpl->GetFunction());
+  }
+
+private:
+  static NAN_METHOD(ctor) {
+    HandleScope scope;
+
+    Local<Object> opt = info[0]->ToObject();
+
+    Utf8String path(opt->Get(New("path").ToLocalChecked()));
+    Utf8String topicName(opt->Get(New("topic").ToLocalChecked()));
+
+    TopicOpt topicOpt{ 1024 * 1024 * 1024, 8 };
+    Local<Value> chunkSize = opt->Get(New("chunkSize").ToLocalChecked());
+    Local<Value> chunksToKeep = opt->Get(New("chunksToKeep").ToLocalChecked());
+    Local<Value> bgFlush = opt->Get(New("backgroundFlush").ToLocalChecked());
+    if (chunkSize->IsNumber()) topicOpt.chunkSize = size_t(chunkSize->NumberValue());
+    if (chunksToKeep->IsNumber()) topicOpt.chunksToKeep = size_t(chunksToKeep->NumberValue());
+
+    ProducerWrap* ptr = new ProducerWrap(*path, *topicName, &topicOpt, bgFlush->BooleanValue());
+    ptr->Wrap(info.This());
+    info.GetReturnValue().Set(info.This());
+  }
+
+  template<DataType DT> static NAN_METHOD(push) {
+    HandleScope scope;
+
+    ProducerWrap* ptr = ObjectWrap::Unwrap<ProducerWrap>(info.This());
+
+    BatchWrap<DT> batch;
+    batch.reserve(info.Length());
+    for (int i = 0; i < info.Length(); i++) {
+      batch.push(info[i]);
     }
 
-  private:
-    static NAN_METHOD(ctor) {
-        NanScope();
+    ptr->_handle.push(batch.get());
 
-        Handle<Object> opt = args[0]->ToObject();
+    info.GetReturnValue().Set(Null());
+  }
 
-        NanUtf8String path(opt->Get(NanNew("path")));
-        NanUtf8String topicName(opt->Get(NanNew("topic")));
+  template<DataType DT> static NAN_METHOD(push2Cache) {
+    HandleScope scope;
 
-        TopicOpt topicOpt{ 1024 * 1024 * 1024, 8 };
-        Local<Value> chunkSize = opt->Get(NanNew("chunkSize"));
-        Local<Value> chunksToKeep = opt->Get(NanNew("chunksToKeep"));
-        Local<Value> bgFlush = opt->Get(NanNew("backgroundFlush"));
-        if (chunkSize->IsNumber()) topicOpt.chunkSize = size_t(chunkSize->NumberValue());
-        if (chunksToKeep->IsNumber()) topicOpt.chunksToKeep = size_t(chunksToKeep->NumberValue());
+    ProducerWrap* ptr = ObjectWrap::Unwrap<ProducerWrap>(info.This());
 
-        ProducerWrap* ptr = new ProducerWrap(*path, *topicName, &topicOpt, bgFlush->BooleanValue());
-        ptr->Wrap(args.This());
-        NanReturnValue(args.This());
+    BatchWrap<DT> batch;
+    batch.reserve(info.Length());
+    for (int i = 0; i < info.Length(); i++) {
+      batch.push(info[i]);
     }
 
-    template<DataType DT> static NAN_METHOD(push) {
-        NanScope();
+    ptr->_handle.push2Cache(batch.get());
 
-        ProducerWrap* ptr = ObjectWrap::Unwrap<ProducerWrap>(args.This());
+    info.GetReturnValue().Set(Null());
+  }
 
-        BatchWrap<DT> batch;
-        batch.reserve(args.Length());
-        for (int i = 0; i < args.Length(); i++) {
-            batch.push(args[i]);
-        }
+private:
+  ProducerWrap(const char* path, const char* name, TopicOpt* opt, bool bgFlush) : _handle(path, name, opt) {
+    if (bgFlush) _handle.enableBackgroundFlush(std::chrono::milliseconds(200));
+  }
 
-        ptr->_handle.push(batch.get());
-
-        NanReturnUndefined();
-    }
-
-    template<DataType DT> static NAN_METHOD(push2Cache) {
-        NanScope();
-
-        ProducerWrap* ptr = ObjectWrap::Unwrap<ProducerWrap>(args.This());
-
-        BatchWrap<DT> batch;
-        batch.reserve(args.Length());
-        for (int i = 0; i < args.Length(); i++) {
-            batch.push(args[i]);
-        }
-
-        ptr->_handle.push2Cache(batch.get());
-
-        NanReturnUndefined();
-    }
-
-  private:
-    ProducerWrap(const char* path, const char* name, TopicOpt* opt, bool bgFlush) : _handle(path, name, opt) {
-        if (bgFlush) _handle.enableBackgroundFlush(chrono::milliseconds(200));
-    }
-
-    Producer _handle;
+  Producer _handle;
 };
 
 template<DataType DT> class ReturnMaker {
-  public:
-    Local<Value> static make(const Consumer::ItemType& item);
+public:
+  Local<Value> static make(const Consumer::ItemType& item);
 };
 
 template<> Local<Value> ReturnMaker<STRING_TYPE>::make(const Consumer::ItemType& item) {
-    return NanNew(std::get<1>(item));
+  return New(std::get<1>(item)).ToLocalChecked();
 }
 
 template<> Local<Value> ReturnMaker<BUFFER_TYPE>::make(const Consumer::ItemType& item) {
-    return NanNewBufferHandle(std::get<1>(item), std::get<2>(item));
+  return NewBuffer((char*)std::get<1>(item), std::get<2>(item)).ToLocalChecked();
 }
 
 class ConsumerWrap : public ObjectWrap {
-  public:
-    static void setup(Handle<Object>& exports) {
-        const char* className = "Consumer";
+public:
+  static void setup(Local<Object> exports) {
+    const char* className = "Consumer";
 
-        Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(ConsumerWrap::ctor);
-        tpl->SetClassName(NanNew(className));
-        tpl->InstanceTemplate()->SetInternalFieldCount(1);
+    Local<FunctionTemplate> tpl = New<FunctionTemplate>(ctor);
+    tpl->SetClassName(New(className).ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-        NODE_SET_PROTOTYPE_METHOD(tpl, "offset", ConsumerWrap::offset);
-        NODE_SET_PROTOTYPE_METHOD(tpl, "popString", ConsumerWrap::pop<STRING_TYPE>);
-        NODE_SET_PROTOTYPE_METHOD(tpl, "popBuffer", ConsumerWrap::pop<BUFFER_TYPE>);
+    SetPrototypeMethod(tpl, "offset", offset);
+    SetPrototypeMethod(tpl, "popString", pop<STRING_TYPE>);
+    SetPrototypeMethod(tpl, "popBuffer", pop<BUFFER_TYPE>);
 
-        exports->Set(NanNew(className), tpl->GetFunction());
+    exports->Set(New(className).ToLocalChecked(), tpl->GetFunction());
+  }
+
+private:
+  static NAN_METHOD(ctor) {
+    HandleScope scope;
+
+    Local<Object> opt = info[0]->ToObject();
+
+    Utf8String path(opt->Get(New("path").ToLocalChecked()));
+    Utf8String topicName(opt->Get(New("topic").ToLocalChecked()));
+    Utf8String name(opt->Get(New("name").ToLocalChecked()));
+
+    TopicOpt topicOpt{ 1024 * 1024 * 1024, 0 };
+    Local<Value> chunkSize = opt->Get(New("chunkSize").ToLocalChecked());
+    if (chunkSize->IsNumber()) topicOpt.chunkSize = size_t(chunkSize->NumberValue());
+
+    ConsumerWrap* ptr = new ConsumerWrap(*path, *topicName, *name, &topicOpt);
+    Local<Value> batchSize = opt->Get(New("batchSize").ToLocalChecked());
+    if (batchSize->IsNumber()) {
+      size_t bs = size_t(batchSize->NumberValue());
+      if (bs > 0 && bs < 1024 * 1024) ptr->_batchSize = bs;
     }
 
-  private:
-    static NAN_METHOD(ctor) {
-        NanScope();
+    ptr->Wrap(info.This());
+    info.GetReturnValue().Set(info.This());
+  }
 
-        Handle<Object> opt = args[0]->ToObject();
+  static NAN_METHOD(offset) {
+    HandleScope scope;
 
-        NanUtf8String path(opt->Get(NanNew("path")));
-        NanUtf8String topicName(opt->Get(NanNew("topic")));
-        NanUtf8String name(opt->Get(NanNew("name")));
+    ConsumerWrap* ptr = ObjectWrap::Unwrap<ConsumerWrap>(info.This());
 
-        TopicOpt topicOpt{ 1024 * 1024 * 1024, 0 };
-        Local<Value> chunkSize = opt->Get(NanNew("chunkSize"));
-        if (chunkSize->IsNumber()) topicOpt.chunkSize = size_t(chunkSize->NumberValue());
-
-        ConsumerWrap* ptr = new ConsumerWrap(*path, *topicName, *name, &topicOpt);
-        Local<Value> batchSize = opt->Get(NanNew("batchSize"));
-        if (batchSize->IsNumber()) {
-            size_t bs = size_t(batchSize->NumberValue());
-            if (bs > 0 && bs < 1024 * 1024) ptr->_batchSize = bs;
-        }
-
-        ptr->Wrap(args.This());
-        NanReturnValue(args.This());
+    if (ptr->_cur <= ptr->_batch.size()) {
+      info.GetReturnValue().Set((New(double(std::get<0>(ptr->_batch.at(ptr->_cur - 1))))));
     }
 
-    static NAN_METHOD(offset) {
-        NanScope();
+    info.GetReturnValue().Set(Null());
+  }
 
-        ConsumerWrap* ptr = ObjectWrap::Unwrap<ConsumerWrap>(args.This());
+  template<DataType DT> static NAN_METHOD(pop) {
+    HandleScope scope;
 
-        if (ptr->_cur <= ptr->_batch.size()) {
-            NanReturnValue(NanNew(double(std::get<0>(ptr->_batch.at(ptr->_cur - 1)))));
-        }
+    ConsumerWrap* ptr = ObjectWrap::Unwrap<ConsumerWrap>(info.This());
 
-        NanReturnUndefined();
+    if (ptr->_cur < ptr->_batch.size()) {
+      info.GetReturnValue().Set(ReturnMaker<DT>::make(ptr->_batch.at(ptr->_cur++)));
     }
 
-    template<DataType DT> static NAN_METHOD(pop) {
-        NanScope();
-
-        ConsumerWrap* ptr = ObjectWrap::Unwrap<ConsumerWrap>(args.This());
-
-        if (ptr->_cur < ptr->_batch.size()) {
-            NanReturnValue(ReturnMaker<DT>::make(ptr->_batch.at(ptr->_cur++)));
-        }
-
-        ptr->_batch.clear();
-        ptr->_cur = 1;
-        ptr->_handle.pop(ptr->_batch, ptr->_batchSize);
-        if (ptr->_batch.size() > 0) {
-            NanReturnValue(ReturnMaker<DT>::make(ptr->_batch.at(0)));
-        }
-
-        NanReturnUndefined();
+    ptr->_batch.clear();
+    ptr->_cur = 1;
+    ptr->_handle.pop(ptr->_batch, ptr->_batchSize);
+    if (ptr->_batch.size() > 0) {
+      info.GetReturnValue().Set(ReturnMaker<DT>::make(ptr->_batch.at(0)));
     }
 
-  private:
-    ConsumerWrap(const char* path, const char* topicName, const char* name, TopicOpt* opt) : _handle(path, topicName, name, opt), _cur(0), _batchSize(128) { }
+    info.GetReturnValue().Set(Null());
+  }
 
-    Consumer _handle;
-    Consumer::BatchType _batch;
-    size_t _cur, _batchSize;
+private:
+  ConsumerWrap(const char* path, const char* topicName, const char* name, TopicOpt* opt) : _handle(path, topicName, name, opt), _cur(0), _batchSize(128) { }
+
+  Consumer _handle;
+  Consumer::BatchType _batch;
+  size_t _cur, _batchSize;
 };
 
 class TopicWrap : public ObjectWrap {
-  public:
-    static void setup(Handle<Object>& exports) {
-        const char* className = "Topic";
+public:
+  static void setup(Local<Object> exports) {
+    const char* className = "Topic";
 
-        Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(TopicWrap::ctor);
-        tpl->SetClassName(NanNew(className));
-        tpl->InstanceTemplate()->SetInternalFieldCount(1);
+    Local<FunctionTemplate> tpl = New<FunctionTemplate>(TopicWrap::ctor);
+    tpl->SetClassName(New(className).ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-        NODE_SET_PROTOTYPE_METHOD(tpl, "status", TopicWrap::status);
+    SetPrototypeMethod(tpl, "status", status);
 
-        exports->Set(NanNew(className), tpl->GetFunction());
+    exports->Set(New(className).ToLocalChecked(), tpl->GetFunction());
+  }
+
+private:
+  static NAN_METHOD(ctor) {
+    HandleScope scope;
+
+    Local<Object> opt = info[0]->ToObject();
+
+    Utf8String path(opt->Get(New("path").ToLocalChecked()));
+    Utf8String topicName(opt->Get(New("topic").ToLocalChecked()));
+
+    TopicWrap *ptr = new TopicWrap(*path, *topicName);
+
+    ptr->Wrap(info.This());
+    info.GetReturnValue().Set(Null());
+  }
+
+  static NAN_METHOD(status) {
+    HandleScope scope;
+
+    TopicWrap* ptr = ObjectWrap::Unwrap<TopicWrap>(info.This());
+    TopicStatus st = ptr->_handle->status();
+
+    Local<Object> ret = New<Object>();
+    ret->Set(New("producerHead").ToLocalChecked(), New<Number>(double(st.producerHead)));
+
+    Local<Object> consumerHeads = New<Object>();
+    for (auto it : st.consumerHeads) {
+      consumerHeads->Set(New(it.first).ToLocalChecked(), New<Number>(double(it.second)));
     }
+    ret->Set(New("consumerHeads").ToLocalChecked(), consumerHeads);
 
-  private:
-    static NAN_METHOD(ctor) {
-        NanScope();
+    info.GetReturnValue().Set(ret);
+  }
 
-        Handle<Object> opt = args[0]->ToObject();
+private:
+  TopicWrap(const char* path, const char* topicName) : _handle(EnvManager::getEnv(path)->getTopic(topicName)) {
 
-        NanUtf8String path(opt->Get(NanNew("path")));
-        NanUtf8String topicName(opt->Get(NanNew("topic")));
+  }
 
-        TopicWrap *ptr = new TopicWrap(*path, *topicName);
-
-        ptr->Wrap(args.This());
-        NanReturnValue(args.This());
-    }
-
-    static NAN_METHOD(status) {
-        NanScope();
-
-        TopicWrap* ptr = ObjectWrap::Unwrap<TopicWrap>(args.This());
-        TopicStatus st = ptr->_handle->status();
-
-        Local<Object> ret = NanNew<Object>();
-        ret->Set(NanNew("producerHead"), NanNew<Number>(double(st.producerHead)));
-
-        Local<Object> consumerHeads = NanNew<Object>();
-        for (auto it : st.consumerHeads) {
-            consumerHeads->Set(NanNew(it.first), NanNew<Number>(double(it.second)));
-        }
-        ret->Set(NanNew("consumerHeads"), consumerHeads);
-
-        NanReturnValue(ret);
-    }
-
-  private:
-    TopicWrap(const char* path, const char* topicName) : _handle(EnvManager::getEnv(path)->getTopic(topicName)) {
-
-    }
-
-  private:
-    Topic *_handle;
+private:
+  Topic *_handle;
 };
 
-void init(v8::Handle<v8::Object> exports) {
-    exports->Set(NanNew("STRING_TYPE"), NanNew(STRING_TYPE));
-    exports->Set(NanNew("BUFFER_TYPE"), NanNew(BUFFER_TYPE));
-
-    TopicWrap::setup(exports);
-    ProducerWrap::setup(exports);
-    ConsumerWrap::setup(exports);
+void init(v8::Local<v8::Object> exports) {
+  exports->Set(Nan::New("STRING_TYPE").ToLocalChecked(), Nan::New(STRING_TYPE));
+  exports->Set(Nan::New("BUFFER_TYPE").ToLocalChecked(), Nan::New(BUFFER_TYPE));
+  TopicWrap::setup(exports);
+  ProducerWrap::setup(exports);
+  ConsumerWrap::setup(exports);
 }
 
 NODE_MODULE(lmdb_queue, init);
--- src/producer.cc
+++ src/producer.cc
@@ -13,224 +13,224 @@
 using namespace std;
 
 Producer::ItemType Producer::ItemType::create(size_t len) {
-    ItemType ret(new char[len], len);
-    ret._shouldDelete = true;
-    return std::move(ret);
+  ItemType ret(new char[len], len);
+  ret._shouldDelete = true;
+  return ret;
 }
 
 Producer::ItemType::~ItemType() {
-    if (_shouldDelete) {
-        delete _mem;
-    }
+  if (_shouldDelete) {
+    delete _mem;
+  }
 }
 
 Producer::Producer(const string& root, const string& topic, TopicOpt* opt, size_t cacheMax) : _topic(EnvManager::getEnv(root)->getTopic(topic)), _current(-1), _env(nullptr), _db(0), _bgEnabled(false), _bgRunning(false), _cacheMax(cacheMax), _cacheCurrent(&_cache0) {
-    if (opt) {
-        _opt = *opt;
-    } else {
-        /* Default opt */
-        _opt.chunkSize = 1024 * 1024 * 1024;
-        _opt.chunksToKeep = 8;
-    }
+  if (opt) {
+    _opt = *opt;
+  } else {
+    /* Default opt */
+    _opt.chunkSize = 1024 * 1024 * 1024;
+    _opt.chunksToKeep = 8;
+  }
 
-    Txn txn(_topic->getEnv(), NULL);
-    openHead(&txn);
-    txn.commit();
+  Txn txn(_topic->getEnv(), NULL);
+  openHead(&txn);
+  txn.commit();
 
-    _cache0.reserve(_cacheMax);
+  _cache0.reserve(_cacheMax);
 }
 
 Producer::~Producer() {
-    if (_bgEnabled) {
-        _bgRunning = false;
-        {
-            unique_lock<mutex> lck(_flushMtx);
-            _bgCv.notify_one();
-        }
-        _bgFlush.join();
-    } else {
-        flush();
+  if (_bgEnabled) {
+    _bgRunning = false;
+    {
+      unique_lock<mutex> lck(_flushMtx);
+      _bgCv.notify_one();
     }
+    _bgFlush.join();
+  } else {
+    flush();
+  }
 
-    closeCurrent();
+  closeCurrent();
 }
 
 bool Producer::enableBackgroundFlush(chrono::milliseconds flushInterval) {
-    if (!_bgEnabled) {
-        if (flushInterval < chrono::milliseconds(2)) {
-            cout << "LMDB_QUEUE WARNING: Background flush interval cannot less than 2ms." << endl;
-            flushInterval = chrono::milliseconds(2);
-        }
-
-        _bgEnabled = true;
-        _bgRunning = true;
-        _cache1.reserve(_cacheMax);
-        _flushInterval = flushInterval;
-        _bgFlush = thread(bind(&Producer::flushWorker, this));
-        return true;
-    } else {
-        cout << "LMDB_QUEUE WARNING: Background flush thread already started." << endl;
-        return false;
+  if (!_bgEnabled) {
+    if (flushInterval < chrono::milliseconds(2)) {
+      cout << "LMDB_QUEUE WARNING: Background flush interval cannot less than 2ms." << endl;
+      flushInterval = chrono::milliseconds(2);
     }
+
+    _bgEnabled = true;
+    _bgRunning = true;
+    _cache1.reserve(_cacheMax);
+    _flushInterval = flushInterval;
+    _bgFlush = thread(bind(&Producer::flushWorker, this));
+    return true;
+  } else {
+    cout << "LMDB_QUEUE WARNING: Background flush thread already started." << endl;
+    return false;
+  }
 }
 
 bool Producer::push(const Producer::BatchType& batch) {
-    bool isFull = false;
+  bool isFull = false;
 
-    {
-        Txn txn(_topic->getEnv(), _env);
+  {
+    Txn txn(_topic->getEnv(), _env);
 
-        uint64_t head = _topic->getProducerHead(txn);
-        for (auto& item : batch) {
-            MDB_val key{ sizeof(head), &++head },
-                    val{ item.len(), (void*)item.data() };
+    uint64_t head = _topic->getProducerHead(txn);
+    for (auto& item : batch) {
+      MDB_val key{ sizeof(head), &++head },
+        val{ item.len(), (void*)item.data() };
 
-                    int rc = mdb_put(txn.getTxn(), _db, &key, &val, MDB_APPEND);
-                    if (rc == MDB_MAP_FULL) {
-                        txn.abort();
-                        isFull = true;
-                        break;
-                    }
-        }
-
-        if (!isFull) {
-            _topic->setProducerHead(txn, head);
-            int rc = txn.commit();
-            if (rc == MDB_MAP_FULL) {
-                isFull = true;
-            }
+        int rc = mdb_put(txn.getTxn(), _db, &key, &val, MDB_APPEND);
+        if (rc == MDB_MAP_FULL) {
+          txn.abort();
+          isFull = true;
+          break;
         }
     }
 
-    if (isFull) {
-        rotate();
-        return push(batch);
+    if (!isFull) {
+      _topic->setProducerHead(txn, head);
+      int rc = txn.commit();
+      if (rc == MDB_MAP_FULL) {
+        isFull = true;
+      }
     }
+  }
 
-    return true;
+  if (isFull) {
+    rotate();
+    return push(batch);
+  }
+
+  return true;
 }
 
 void Producer::setCacheSize(size_t sz) {
-    std::lock_guard<std::mutex> guard(_cacheMtx);
-    _cacheMax = sz;
+  std::lock_guard<std::mutex> guard(_cacheMtx);
+  _cacheMax = sz;
 
-    if (_cacheCurrent->size() > sz) {
-        flushImpl();
-    }
+  if (_cacheCurrent->size() > sz) {
+    flushImpl();
+  }
 }
 
 void Producer::push2Cache(BatchType& batch) {
-    std::lock_guard<std::mutex> guard(_cacheMtx);
+  std::lock_guard<std::mutex> guard(_cacheMtx);
 
-    for (auto& item : batch) {
-        _cacheCurrent->push_back(std::move(item));
-    }
+  for (auto& item : batch) {
+    _cacheCurrent->push_back(std::move(item));
+  }
 
-    batch.clear();
+  batch.clear();
 
-    if (_cacheCurrent->size() >= _cacheMax) {
-        flushImpl();
-    }
+  if (_cacheCurrent->size() >= _cacheMax) {
+    flushImpl();
+  }
 }
 
 void Producer::push2Cache(ItemType&& item) {
-    std::lock_guard<std::mutex> guard(_cacheMtx);
-    _cacheCurrent->push_back(std::move(item));
-    if (_cacheCurrent->size() >= _cacheMax) {
-        flushImpl();
-    }
+  std::lock_guard<std::mutex> guard(_cacheMtx);
+  _cacheCurrent->push_back(std::move(item));
+  if (_cacheCurrent->size() >= _cacheMax) {
+    flushImpl();
+  }
 }
 
 void Producer::flush() {
-    std::lock_guard<std::mutex> guard(_cacheMtx);
-    flushImpl();
+  std::lock_guard<std::mutex> guard(_cacheMtx);
+  flushImpl();
 }
 
 void Producer::flushWorker() {
-    while (_bgRunning) {
-        {
-            unique_lock<mutex> lck(_flushMtx);
-            _bgCv.wait_for(lck, _flushInterval);
-        }
-
-        BatchType *flush = nullptr;
-
-        {
-            lock_guard<mutex> guard(_cacheMtx);
-            if (_cacheCurrent->size() > 0) {
-                flush = _cacheCurrent;
-                _cacheCurrent = _cacheCurrent == &_cache0 ? &_cache1 : &_cache0;
-            }
-        }
-
-        if (flush) {
-            push(*flush);
-            flush->clear();
-        }
+  while (_bgRunning) {
+    {
+      unique_lock<mutex> lck(_flushMtx);
+      _bgCv.wait_for(lck, _flushInterval);
     }
+
+    BatchType *flush = nullptr;
+
+    {
+      lock_guard<mutex> guard(_cacheMtx);
+      if (_cacheCurrent->size() > 0) {
+        flush = _cacheCurrent;
+        _cacheCurrent = _cacheCurrent == &_cache0 ? &_cache1 : &_cache0;
+      }
+    }
+
+    if (flush) {
+      push(*flush);
+      flush->clear();
+    }
+  }
 }
 
 void Producer::flushImpl() {
-    if (_cacheCurrent->size() > 0) {
-        if (_bgEnabled) {
-            unique_lock<mutex> lck(_flushMtx);
-            _bgCv.notify_one();
-        } else {
-            push(*_cacheCurrent);
-            _cacheCurrent->clear();
-        }
+  if (_cacheCurrent->size() > 0) {
+    if (_bgEnabled) {
+      unique_lock<mutex> lck(_flushMtx);
+      _bgCv.notify_one();
+    } else {
+      push(*_cacheCurrent);
+      _cacheCurrent->clear();
     }
+  }
 }
 
 void Producer::closeCurrent() {
-    mdb_dbi_close(_env, _db);
-    mdb_env_close(_env);
+  mdb_dbi_close(_env, _db);
+  mdb_env_close(_env);
 }
 
 void Producer::openHead(Txn* txn, bool rotating) {
-    uint32_t headFile = _topic->getProducerHeadFile(*txn);
-    if (rotating && _current == headFile) {
-        _topic->setProducerHeadFile(*txn, ++headFile, _topic->getProducerHead(*txn) + 1);
-    }
+  uint32_t headFile = _topic->getProducerHeadFile(*txn);
+  if (rotating && _current == headFile) {
+    _topic->setProducerHeadFile(*txn, ++headFile, _topic->getProducerHead(*txn) + 1);
+  }
 
-    _current = headFile;
+  _current = headFile;
 
-    char path[4096];
-    memset(path, '\0', 4096);
-    _topic->getChunkFilePath(path, headFile);
+  char path[4096];
+  memset(path, '\0', 4096);
+  _topic->getChunkFilePath(path, headFile);
 
 #ifdef _WIN32
-    Sleep(500); // Fix error on windows when multi process rotate at same time. ("The requested operation cannot be performed on a file with a user-mapped section open.")
+  Sleep(500); // Fix error on windows when multi process rotate at same time. ("The requested operation cannot be performed on a file with a user-mapped section open.")
 #endif
-    mdb_env_create(&_env);
-    mdb_env_set_mapsize(_env, _opt.chunkSize);
-    int rc = mdb_env_open(_env, path, MDB_NOSYNC | MDB_NOSUBDIR, 0664);
+  mdb_env_create(&_env);
+  mdb_env_set_mapsize(_env, _opt.chunkSize);
+  int rc = mdb_env_open(_env, path, MDB_NOSYNC | MDB_NOSUBDIR, 0664);
 
-    int cleared = 0;
-    mdb_reader_check(_env, &cleared);
+  int cleared = 0;
+  mdb_reader_check(_env, &cleared);
 
-    if (rc != 0) {
-        mdb_env_close(_env);
-        _env = nullptr;
-        printf("Producer open error.\n%s\n", mdb_strerror(rc));
-        return;
-    }
+  if (rc != 0) {
+    mdb_env_close(_env);
+    _env = nullptr;
+    printf("Producer open error.\n%s\n", mdb_strerror(rc));
+    return;
+  }
 
-    MDB_txn *otxn;
-    mdb_txn_begin(_env, NULL, 0, &otxn);
-    mdb_dbi_open(otxn, NULL, MDB_CREATE, &_db);
-    mdb_set_compare(otxn, _db, mdbIntCmp<uint64_t>);
-    mdb_txn_commit(otxn);
+  MDB_txn *otxn;
+  mdb_txn_begin(_env, NULL, 0, &otxn);
+  mdb_dbi_open(otxn, NULL, MDB_CREATE, &_db);
+  mdb_set_compare(otxn, _db, mdbIntCmp<uint64_t>);
+  mdb_txn_commit(otxn);
 }
 
 void Producer::rotate() {
-    Txn txn(_topic->getEnv(), NULL);
+  Txn txn(_topic->getEnv(), NULL);
 
-    closeCurrent();
-    for (size_t chunks = _topic->countChunks(txn); chunks >= _opt.chunksToKeep; --chunks) {
-        _topic->removeOldestChunk(txn);
-    }
+  closeCurrent();
+  for (size_t chunks = _topic->countChunks(txn); chunks >= _opt.chunksToKeep; --chunks) {
+    _topic->removeOldestChunk(txn);
+  }
 
-    openHead(&txn, true);
-    txn.commit();
+  openHead(&txn, true);
+  txn.commit();
 }
